03bfb46d7a947d8ee994d4545cb9303280065c09
Ruben Aguilar <ThatRubenAguilar@gmail.com>
2017-10-10 00:17:08 -0700

    Fix tab bg, try grid bg for details view

--- app/src/main/java/com/innovations/aguilar/pocketstrats/ui/view/MapSpawnTabLayout.java
+++ app/src/main/java/com/innovations/aguilar/pocketstrats/ui/view/MapSpawnTabLayout.java
@@ -8,7 +8,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import com.innovations.aguilar.pocketstrats.R;
@@ -16,6 +18,8 @@ import com.innovations.aguilar.pocketstrats.sql.dto.MapDataDTO;
 import com.innovations.aguilar.pocketstrats.sql.dto.SpawnSide;
 import com.innovations.aguilar.pocketstrats.ui.CustomTypeFaces;
 import com.innovations.aguilar.pocketstrats.ui.adapter.MapSearchItemAdapter;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Ruben on 9/30/2017.
@@ -55,28 +59,34 @@ public class MapSpawnTabLayout extends LinearLayout {
         super.onFinishInflate();
 
 
-        spawnTabs.addTab(configureTab(spawnTabs.newTab(), SpawnSide.Attack));
-        spawnTabs.addTab(configureTab(spawnTabs.newTab(),SpawnSide.Defend));
+        spawnTabs.addTab(configureTab(spawnTabs.newTab(), SpawnSide.Attack, true));
+        spawnTabs.addTab(configureTab(spawnTabs.newTab(), SpawnSide.Defend, false));
     }
 
 
-    private TabLayout.Tab configureTab(TabLayout.Tab sideTab, SpawnSide side) {
+    private TabLayout.Tab configureTab(TabLayout.Tab sideTab, SpawnSide side, boolean selected) {
         Context context = getContext();
         TextView tabButton = (TextView)inflate(context, R.layout.tab_button_layout, null);
         tabButton.setTypeface(CustomTypeFaces.BigNoodleTitlingOblique(context.getAssets()));
+
+        // TODO: wtf with the tab background, make grid bg for tips, added app ns to tab layout,
+        ViewGroup.LayoutParams layoutParams = new TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        tabButton.setLayoutParams(layoutParams);
+
         tabButton.setText(side.toString());
-        Drawable wrappedDrawable = ContextCompat.getDrawable(context, R.drawable.tab_toggle);
-        tabButton.setBackground(DrawableCompat.unwrap(wrappedDrawable));
-        // TODO: wtf with the tab background, make grid bg for tips, added app ns to tab layout
-
-
-        // TODO: Inflate custom textviews and setup like mapIcon.
-        // See also: https://stackoverflow.com/questions/31640563/how-do-i-change-a-tab-background-color-when-using-tablayout
         sideTab.setCustomView(tabButton);
-
         sideTab.setText(side.toString());
 
+        setBackgroundSelector(sideTab, selected);
+
         return sideTab;
+    }
+
+    private void setBackgroundSelector(TabLayout.Tab sideTab, boolean selected) {
+        TextView tabButton = (TextView)sideTab.getCustomView();
+        int tabButtonId = selected ? R.drawable.tab_button_on : R.drawable.tab_button_off;
+        Drawable wrappedDrawable = ContextCompat.getDrawable(getContext(), tabButtonId);
+        tabButton.setBackground(DrawableCompat.unwrap(wrappedDrawable));
     }
 
     public void setMapIcon(MapDataDTO map) {
@@ -95,13 +105,15 @@ public class MapSpawnTabLayout extends LinearLayout {
         TabLayout.OnTabSelectedListener tabListener = new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                SpawnSide side = toSpawnSide(tab);;
+                setBackgroundSelector(tab, true);
+                SpawnSide side = toSpawnSide(tab);
                 listener.onTabSelected(tab, side);
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-                SpawnSide side = toSpawnSide(tab);;
+                setBackgroundSelector(tab, false);
+                SpawnSide side = toSpawnSide(tab);
                 listener.onTabUnselected(tab, side);
             }
 
@@ -124,6 +136,10 @@ public class MapSpawnTabLayout extends LinearLayout {
     }
 
     private static SpawnSide toSpawnSide(TabLayout.Tab tab) {
+        if (tab.getCustomView() != null) {
+            TextView customView = (TextView) tab.getCustomView();
+            return SpawnSide.valueOf(customView.getText().toString());
+        }
         return SpawnSide.valueOf(tab.getText().toString());
     }
 }
